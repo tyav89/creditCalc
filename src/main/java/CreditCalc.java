2f97e4ca690ecf7219612036d7523ad02cda199f
Dmitriy Tyavin <tyav89@mail.ru>
2021-01-11 16:22:02 +0300

    Added method implementation

--- src/main/java/CreditCalc.java
+++ src/main/java/CreditCalc.java
@@ -1,3 +1,13 @@
 public class CreditCalc {
+    public int monthlyPayment(int loanAmount, double interestRate, int loanPeriod){
+        return (int) Math.round (loanAmount * (interestRate + ( interestRate/ (Math.pow((1 + interestRate), loanPeriod) - 1))));
+    }
 
+    public int totalAmount(int loanAmount, double interestRate, int loanPeriod){
+        return monthlyPayment(loanAmount, interestRate, loanPeriod) * loanPeriod;
+    }
+
+    public int overpaymentCalc(int loanAmount, double interestRate, int loanPeriod){
+        return totalAmount(loanAmount, interestRate, loanPeriod) - loanAmount;
+    }
 }
